2b483496c3c0547a0ea4c47c80aa89ad9fc71b64
Bhuwan Bhandari <bhuwanbhandari2821@gmail.com>
2024-10-03 10:32:29 -0400

    add: contributors name

--- index.php
+++ index.php
@@ -7,7 +7,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>BizBlog</title>
     <link rel="shortcut icon" type="image/png" href="assets/images/favicon.png">
-    <link href="https://fonts.googleapis.com/css?family=Quicksand:300,400,500%7CSpectral:400,400i,500,600,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Quicksand:300,400,500%7CSpectral:400,400i,500,600,700"
+        rel="stylesheet">
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/font-awesome.min.css">
     <link rel="stylesheet" href="assets/plugins/animate/animate.min.css">
@@ -29,8 +30,10 @@
     </div>
     <div class="nav-search-box">
         <form>
-            <div class="input-group"> <input type="text" class="form-control" placeholder="eg. feel the love and …"> <span class="b-line"></span> <span class="b-line-under"></span>
-                <div class="input-group-append"> <button type="button" class="btn"> <img src="assets/images/search-icon.svg" alt="" class="img-fluid svg"> </button> </div>
+            <div class="input-group"> <input type="text" class="form-control" placeholder="eg. feel the love and …">
+                <span class="b-line"></span> <span class="b-line-under"></span>
+                <div class="input-group-append"> <button type="button" class="btn"> <img
+                            src="assets/images/search-icon.svg" alt="" class="img-fluid svg"> </button> </div>
             </div>
         </form>
     </div>
@@ -39,7 +42,8 @@
             <div class="container-fluid pl-120 pr-120 position-relative">
                 <div class="row d-flex align-items-center">
                     <div class="col-lg-3 col-md-4 col-6">
-                        <div class="logo"> <a href="#"><img src="assets/images/logo.png" alt="" class="img-fluid"></a> </div>
+                        <div class="logo"> <a href="#"><img src="assets/images/logo.png" alt="" class="img-fluid"></a>
+                        </div>
                     </div>
                     <div class="col-lg-9 col-md-8 col-6 d-flex justify-content-end position-static">
                         <div class="nav-menu-cover">
@@ -83,9 +87,12 @@
                         </div>
                         <div class="mobile-menu-cover">
                             <ul class="nav mobile-nav-menu">
-                                <li class="search-toggle-open"> <img src="assets/images/search-icon.svg" alt="" class="img-fluid svg"> </li>
-                                <li class="search-toggle-close hide"> <img src="assets/images/close.svg" alt="" class="img-fluid"> </li>
-                                <li class="nav-menu-toggle"> <img src="assets/images/menu-toggler.svg" alt="" class="img-fluid svg"> </li>
+                                <li class="search-toggle-open"> <img src="assets/images/search-icon.svg" alt=""
+                                        class="img-fluid svg"> </li>
+                                <li class="search-toggle-close hide"> <img src="assets/images/close.svg" alt=""
+                                        class="img-fluid"> </li>
+                                <li class="nav-menu-toggle"> <img src="assets/images/menu-toggler.svg" alt=""
+                                        class="img-fluid svg"> </li>
                             </ul>
                         </div>
                     </div>
@@ -116,7 +123,9 @@
                             <h2><a href="blog-details.html">A Funny Thing That Happens In Relationships</a></h2>
                         </div>
                         <ul class="nav meta align-items-center">
-                            <li class="meta-author"> <img src="assets/images/blog/author.jpg" alt="" class="img-fluid"> <a href="#">Alex Garry</a> </li>
+                            <li class="meta-author"> <img src="assets/images/blog/author.jpg" alt="" class="img-fluid">
+                                <a href="#">Alex Garry</a>
+                            </li>
                             <li class="meta-date"><a href="#">2 Feb 2019</a></li>
                             <li class="meta-comments"><a href="#"><i class="fa fa-comment"></i> 2</a></li>
                         </ul>
@@ -134,7 +143,9 @@
                             <h2><a href="blog-details.html">The One Thing I Do When Fashion Come Over</a></h2>
                         </div>
                         <ul class="nav meta align-items-center">
-                            <li class="meta-author"> <img src="assets/images/blog/author.jpg" alt="" class="img-fluid"> <a href="#">Alex Garry</a> </li>
+                            <li class="meta-author"> <img src="assets/images/blog/author.jpg" alt="" class="img-fluid">
+                                <a href="#">Alex Garry</a>
+                            </li>
                             <li class="meta-date"><a href="#">2 Feb 2019</a></li>
                             <li class="meta-comments"><a href="#"><i class="fa fa-comment"></i> 2</a></li>
                         </ul>
@@ -152,7 +163,9 @@
                             <h2><a href="blog-details.html">Summer Adventure Essentials From Backcountry</a></h2>
                         </div>
                         <ul class="nav meta align-items-center">
-                            <li class="meta-author"> <img src="assets/images/blog/author.jpg" alt="" class="img-fluid"> <a href="#">Alex Garry</a> </li>
+                            <li class="meta-author"> <img src="assets/images/blog/author.jpg" alt="" class="img-fluid">
+                                <a href="#">Alex Garry</a>
+                            </li>
                             <li class="meta-date"><a href="#">2 Feb 2019</a></li>
                             <li class="meta-comments"><a href="#"><i class="fa fa-comment"></i> 2</a></li>
                         </ul>
@@ -170,7 +183,9 @@
                             <h2><a href="blog-details.html">Top Things To Look For When Choosing A Safari Lodge</a></h2>
                         </div>
                         <ul class="nav meta align-items-center">
-                            <li class="meta-author"> <img src="assets/images/blog/author.jpg" alt="" class="img-fluid"> <a href="#">Alex Garry</a> </li>
+                            <li class="meta-author"> <img src="assets/images/blog/author.jpg" alt="" class="img-fluid">
+                                <a href="#">Alex Garry</a>
+                            </li>
                             <li class="meta-date"><a href="#">2 Feb 2019</a></li>
                             <li class="meta-comments"><a href="#"><i class="fa fa-comment"></i> 2</a></li>
                         </ul>
@@ -188,7 +203,9 @@
                             <h2><a href="blog-details.html">Blaak Attack Earns Boels First 2019 Victory</a></h2>
                         </div>
                         <ul class="nav meta align-items-center">
-                            <li class="meta-author"> <img src="assets/images/blog/author.jpg" alt="" class="img-fluid"> <a href="#">Alex Garry</a> </li>
+                            <li class="meta-author"> <img src="assets/images/blog/author.jpg" alt="" class="img-fluid">
+                                <a href="#">Alex Garry</a>
+                            </li>
                             <li class="meta-date"><a href="#">2 Feb 2019</a></li>
                             <li class="meta-comments"><a href="#"><i class="fa fa-comment"></i> 2</a></li>
                         </ul>
@@ -206,7 +223,9 @@
                             <h2><a href="blog-details.html">Five Important Facts Should Know About Recipe</a></h2>
                         </div>
                         <ul class="nav meta align-items-center">
-                            <li class="meta-author"> <img src="assets/images/blog/author.jpg" alt="" class="img-fluid"> <a href="#">Alex Garry</a> </li>
+                            <li class="meta-author"> <img src="assets/images/blog/author.jpg" alt="" class="img-fluid">
+                                <a href="#">Alex Garry</a>
+                            </li>
                             <li class="meta-date"><a href="#">2 Feb 2019</a></li>
                             <li class="meta-comments"><a href="#"><i class="fa fa-comment"></i> 2</a></li>
                         </ul>
@@ -221,10 +240,13 @@
                     <div class="post-data">
                         <div class="cats"><a href="category-result.html">Lifestyle</a></div>
                         <div class="title">
-                            <h2><a href="blog-details.html">Great Britain's Winter Olympics Athletes Rated And Slated</a></h2>
+                            <h2><a href="blog-details.html">Great Britain's Winter Olympics Athletes Rated And
+                                    Slated</a></h2>
                         </div>
                         <ul class="nav meta align-items-center">
-                            <li class="meta-author"> <img src="assets/images/blog/author.jpg" alt="" class="img-fluid"> <a href="#">Alex Garry</a> </li>
+                            <li class="meta-author"> <img src="assets/images/blog/author.jpg" alt="" class="img-fluid">
+                                <a href="#">Alex Garry</a>
+                            </li>
                             <li class="meta-date"><a href="#">2 Feb 2019</a></li>
                             <li class="meta-comments"><a href="#"><i class="fa fa-comment"></i> 2</a></li>
                         </ul>
@@ -242,7 +264,9 @@
                             <h2><a href="blog-details.html">Apple Admits To Macbook And Macbook Pro</a></h2>
                         </div>
                         <ul class="nav meta align-items-center">
-                            <li class="meta-author"> <img src="assets/images/blog/author.jpg" alt="" class="img-fluid"> <a href="#">Alex Garry</a> </li>
+                            <li class="meta-author"> <img src="assets/images/blog/author.jpg" alt="" class="img-fluid">
+                                <a href="#">Alex Garry</a>
+                            </li>
                             <li class="meta-date"><a href="#">2 Feb 2019</a></li>
                             <li class="meta-comments"><a href="#"><i class="fa fa-comment"></i> 2</a></li>
                         </ul>
@@ -250,7 +274,8 @@
                 </div>
             </div>
         </div>
-        <div class="post-pagination d-flex justify-content-center"> <span class="current">1</span> <a href="#">2</a> <a href="#">3</a> <a href="#"><i class="fa fa-angle-right"></i></a> </div>
+        <div class="post-pagination d-flex justify-content-center"> <span class="current">1</span> <a href="#">2</a> <a
+                href="#">3</a> <a href="#"><i class="fa fa-angle-right"></i></a> </div>
     </div>
     <section class="newsletter-cover">
         <div class="nl-bg-ol"></div>
@@ -261,11 +286,16 @@
                 </div>
                 <div class="row">
                     <div class="col-lg-8 offset-lg-2">
-                        <form action="https://themelooks.us13.list-manage.com/subscribe/post?u=79f0b132ec25ee223bb41835f&amp;id=f4e0e93d1d" method="post" novalidate>
-                            <div class="input-group"> <input type="text" class="form-control" placeholder="Enter Your Email">
+                        <form
+                            action="https://themelooks.us13.list-manage.com/subscribe/post?u=79f0b132ec25ee223bb41835f&amp;id=f4e0e93d1d"
+                            method="post" novalidate>
+                            <div class="input-group"> <input type="text" class="form-control"
+                                    placeholder="Enter Your Email">
                                 <div class="input-group-append"> <button class="btn btn-default">Submit</button> </div>
                             </div>
-                            <p class="checkbox-cover d-flex justify-content-center"> <label> I've read and accept the <a href="#"> Privacy Policy </a> <input type="checkbox"> <span class="checkmark"></span> </label> </p>
+                            <p class="checkbox-cover d-flex justify-content-center"> <label> I've read and accept the <a
+                                        href="#"> Privacy Policy </a> <input type="checkbox"> <span
+                                        class="checkmark"></span> </label> </p>
                         </form>
                     </div>
                 </div>
@@ -276,9 +306,13 @@
         <div class="container">
             <div class="row align-items-center footer">
                 <div class="col-md-4 text-center text-md-left order-md-1 order-2">
-                    <div class="footer-social"> <a href="#"><i class="fa fa-facebook"></i></a> <a href="#"><i class="fa fa-twitter"></i></a> <a href="#"><i class="fa fa-linkedin"></i></a> <a href="#"><i class="fa fa-google"></i></a> <a href="#"><i class="fa fa-pinterest"></i></a> </div>
+                    <div class="footer-social"> <a href="#"><i class="fa fa-facebook"></i></a> <a href="#"><i
+                                class="fa fa-twitter"></i></a> <a href="#"><i class="fa fa-linkedin"></i></a> <a
+                            href="#"><i class="fa fa-google"></i></a> <a href="#"><i class="fa fa-pinterest"></i></a>
+                    </div>
                 </div>
-                <div class="col-md-4 d-flex justify-content-center order-md-2 order-1"> <a href="index.html"><img src="assets/images/logo.png" alt="" class="img-fluid"></a> </div>
+                <div class="col-md-4 d-flex justify-content-center order-md-2 order-1"> <a href="index.html"><img
+                            src="assets/images/logo.png" alt="" class="img-fluid"></a> </div>
                 <div class="col-md-4 order-md-3 order-3">
                     <div class="footer-cradit text-center text-md-right">
                         <p>© 2019 <a href="index.html">Themelooks.</a></p>
@@ -287,7 +321,8 @@
             </div>
         </div>
     </footer>
-    <div class="back-to-top d-flex align-items-center justify-content-center"> <span><i class="fa fa-long-arrow-up"></i></span> </div>
+    <div class="back-to-top d-flex align-items-center justify-content-center"> <span><i
+                class="fa fa-long-arrow-up"></i></span> </div>
     <script src="assets/js/jquery-1.12.1.min.js"></script>
     <script src="assets/js/bootstrap.bundle.min.js"></script>
     <script src="assets/plugins/owl-carousel/owl.carousel.min.js"></script>
