13230fccc8a91d0589a1c34408545f734f2090c7
Bhuwan Bhandari <bhuwanbhandari2821@gmail.com>
2024-12-03 14:02:39 -0500

    added modal to confirm user deletion

--- admin.php
+++ admin.php
@@ -11,46 +11,75 @@ echo '<pre>';
 
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="shortcut icon" type="image/png" href="assets/images/favicon.png">
     <link href="https://fonts.googleapis.com/css?family=Quicksand:300,400,500%7CSpectral:400,400i,500,600,700"
         rel="stylesheet">
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
     <link rel="stylesheet" href="assets/css/style.css">
-    <link rel="stylesheet" href="assets/css/custom.css">
+
 </head>
 
 <body>
     <div class="container ">
         <h1 class="mb-4">Users</h1>
-        <table class="table table-striped">
-            <thead>
-                <tr>
-                    <th scope="col">No.</th>
-                    <th scope="col">First Name</th>
-                    <th scope="col">Last Name</th>
-                    <th scope="col">Email</th>
-                    <th scope="col">Handle</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                //Looping the users array that has list of all users
-                foreach ($users as $index => $user) { ?>
+        <div class="table-responsive">
+            <table class="table table-striped">
+                <thead>
                     <tr>
-                        <th scope='row'><?php echo $index + 1; ?></th>
-                        <td><?= $user['firstname'] ?></td>
-                        <td><?= $user['lastname'] ?></td>
-                        <td><?= $user['email'] ?></td>
-                        <td>
-                            <a href="edit.php?id= <?= $user['id'] ?>" class="btn btn-primary btn-sm">Edit</a>
-                            <a href="delete.php?id=<?= $user['id'] ?>" class="btn btn-danger btn-sm">Delete</a>
-                        </td>
+                        <th scope="col">No.</th>
+                        <th scope="col">First Name</th>
+                        <th scope="col">Last Name</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Handle</th>
                     </tr>
-                <?php } ?>
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    <?php
+                    //Looping the users array that has list of all users
+                    foreach ($users as $index => $user) { ?>
+                        <tr>
+                            <th scope='row'><?php echo $index + 1; ?></th>
+                            <td><?= $user['firstname'] ?></td>
+                            <td><?= $user['lastname'] ?></td>
+                            <td><?= $user['email'] ?></td>
+                            <td>
+                                <a href="edit.php?id= <?= $user['id'] ?>" class="btn btn-primary btn-sm">Edit</a>
+                                <button type="button" class="btn btn-danger" data-bs-toggle="modal"
+                                    data-bs-target="#exampleModal">
+                                    Delete
+                                </button>
+
+                            </td>
+                        </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+
+        </div>
+
+    </div>
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    Are you sure you want to delete this user?
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-danger">Delete</button>
+                </div>
+            </div>
+        </div>
     </div>
 </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
 </html>
